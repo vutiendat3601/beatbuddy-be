b24043fc89d7813e495c2aae0a79f4f505035dce
vutiendat3601 <vutien.dat.3601@gmail.com>
2024-01-12 22:59:29 +0700

    fix: copy file pg_hba.conf

--- deployment/vps/scripts/install-postgres-ubuntu.sh
+++ deployment/vps/scripts/install-postgres-ubuntu.sh
@@ -12,7 +12,7 @@ apt install -y postgresql-16 postgresql-contrib-16
 mv /etc/postgresql/16/main/pg_ident.conf /etc/postgresql/16/main/pg_ident.conf.orig
 mv /etc/postgresql/16/main/pg_hba.conf /etc/postgresql/16/main/pg_hba.conf.orig
 cp ../auth/db/pg_ident.conf /etc/postgresql/16/main/
-cp ../auth/db/pg_ident.conf /etc/postgresql/16/main/
+cp ../auth/db/pg_hba.conf /etc/postgresql/16/main/
 
 systemctl start postgresql
 systemctl enable postgresql
