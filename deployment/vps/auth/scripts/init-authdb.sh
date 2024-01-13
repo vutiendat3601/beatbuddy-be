f173e03c225f5402a8963c8f34d9dbae992ece87
vutiendat3601 <vutien.dat.3601@gmail.com>
2024-01-13 14:38:05 +0700

    fix: init-authdb SQL

--- deployment/vps/auth/scripts/init-authdb.sh
+++ deployment/vps/auth/scripts/init-authdb.sh
@@ -5,5 +5,5 @@ cp ../db/pg_hba.conf /etc/postgresql/16/main/
 systemctl restart postgresql
 sleep 3s
 
-psql -U postgres -f ../auth/db/init-authdb.sql
-psql -U postgres -f ../auth/db/20240113_04_55_37.sql
+sudo -u vutiendat3601 psql -U postgres -f ../db/init-authdb.sql
+sudo -u vutiendat3601 psql -U beatbuddyauthdb -d beatbuddyauthdb -f ../db/20240113_04_55_37.sql
