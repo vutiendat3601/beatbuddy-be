54402e0a368c3c4e1cc1ae8a2803330b50ce9254
vutiendat3601 <vutien.dat.3601@gmail.com>
2024-01-23 14:55:11 +0700

    chore: backup api database 202401023 14:54:00

--- deployment/docker/api/db/backup.sh
+++ deployment/docker/api/db/backup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-PGPASSWORD=beatbuddyapidb && pg_dump -c -w beatbuddyapidb -U beatbuddyapidb --exclude-table='flyway_schema_history' > /backup/$(date +%Y%m%d'_'%H_%M_%S).sql
+PGPASSWORD=bbapidb && pg_dump -w bbapidb -U bbapidb --exclude-table='flyway_schema_history' > /backup/$(date +%Y%m%d'_'%H_%M_%S).sql
